ea59cfb5bdd5dad052ccbb9a452eb148895c0cd8
Konstantin Polyakov <kostyan_93@mail.ru>
2016-04-25 20:45:23 +0300

    rewrite for Python3

--- captcha_loader_mail.py
+++ captcha_loader_mail.py
@@ -1,22 +1,12 @@
-import urllib2, os, time, common
+import os, time, common
+from urllib import request
 
 captcha_dir = "pics_mail7"
 
 start_i = common.getMaxPicIndex(captcha_dir)
 
-'''for i in range(start_i,start_i+1000):
-  i_str = '0'*(4-len(str(i)))+str(i)
-  image = urllib2.urlopen('https://c.mail.ru/7').read()
-  f = open(os.path.join(captcha_dir, i_str+'.jpg'), 'wb')
-  f.write(image)
-  f.close()
-
-  print i
-  #time.sleep(1)'''
-
-
 def func():
-  captcha = urllib2.urlopen('https://c.mail.ru/7').read()
+  captcha = request.urlopen('https://c.mail.ru/7').read()
   return captcha, True
 
 common.cycle(captcha_dir, start_i, 1000, func)
--- captcha_loader_yandex.py
+++ captcha_loader_yandex.py
@@ -1,30 +1,17 @@
-import urllib2 as u, re, os, common
+import re, os, common
+from urllib import request
 
 #pattern = r'https\:\/\/nb\.captcha\.yandex\.net\/image\?key\=[0-9a-zA-Z]+'
-pattern = r'[a-z]+\.captcha\.yandex\.net\/image\?key=[0-9a-zA-Z]+'
+pattern = rb'[a-z]+\.captcha\.yandex\.net\/image\?key=[0-9a-zA-Z]+'
 captcha_dir = "pics_yandex"
 
 start_i = common.getMaxPicIndex(captcha_dir)
 
-'''for i in range(start_i,start_i+1000):
-  i_str = '0'*(4-len(str(i)))+str(i)
-  page = u.urlopen('https://passport.yandex.ru/registration').read()
-  url = re.findall(pattern, page)
-  #print url
-  if url:
-    f = open(os.path.join(captcha_dir, i_str+'.jpg'), 'wb')
-    captcha = u.urlopen('https://'+url[0]).read()
-    f.write(captcha)
-  else:
-    f = open(os.path.join(captcha_dir, i_str+'.html'), 'wb')
-    f.write(page)
-  f.close()'''
-
 def func():
-  page = u.urlopen('https://passport.yandex.ru/registration').read()
+  page = request.urlopen('https://passport.yandex.ru/registration').read()
   url = re.findall(pattern, page)
   if url:
-    return u.urlopen('https://'+url[0]).read(), True
+    return request.urlopen('https://'+str(url[0], 'utf-8')).read(), True
   else:
     return page, False
 
